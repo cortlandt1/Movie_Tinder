298aabf39313c18dc9cab2b8dff66caa016de591
Gordo <cortland.taylor1@gmail.com>
2022-01-07 11:27:57 -0800

    arrays created and data stored

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Movie_Tinder
 An application to help decide on what to watch
+
+
+API_KEYS
+k_pbtyp0i1
+k_g3c4oo25
--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 
+var filmTitleArray = []
+var filmImgArray = []
+
 function submitButton() {
     event.preventDefault();
     let inputValue = $('#searchQuery').val()
     console.log(inputValue)
-    
-    var searchURL = "https://imdb-api.com/API/AdvancedSearch/k_g3c4oo25/?genres=" + inputValue 
+
+    var searchURL = "https://imdb-api.com/API/AdvancedSearch/k_pbtyp0i1/?genres=" + inputValue
 
     $.ajax({
-        url: searchURL ,
+        url: searchURL,
         method: 'Get',
         dataType: 'json',
     })
 
         .then(function (response) {
             console.log(response)
+            let results = response;
+            let data = results.results
+            console.log(results)
 
-            //   let results = response;
-            //   console.log(results.results[1].title)
-
-            //   var title = results.results[3].title;
-            //   var p = $("<p>").text("Title: " + title);
-
-            //   $('.test').append(p)
+            for (let i = 0; i < data.length; i++) {
+                let title = data[i].title
+                let image = data[i].image
+                filmTitleArray.push(title)
+                filmImgArray.push(image)
+                // console.log(title)
+                // $('.title-container').append("<p>" + title + "</p>");
+                // $('.title-container').append("<img class='img' src=" + image + "></img>");
+            }
         })
+
 }
+
+console.log(filmTitleArray)
+console.log(filmImgArray)
 

de388e263f15a5c336ee2290c0dcd448ace0fd08
Gordo <cortland.taylor1@gmail.com>
2022-01-07 22:08:35 -0800

    testing cycle through array

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function submitButton() {
     let inputValue = $('#searchQuery').val()
     console.log(inputValue)
 
-    var searchURL = "https://imdb-api.com/API/AdvancedSearch/k_pbtyp0i1/?genres=" + inputValue
+    var searchURL = "https://imdb-api.com/API/AdvancedSearch/k_pbtyp0i1/?count=20&genres=" + inputValue
 
     $.ajax({
         url: searchURL,
@@ -26,14 +26,41 @@ function submitButton() {
                 let image = data[i].image
                 filmTitleArray.push(title)
                 filmImgArray.push(image)
-                // console.log(title)
-                // $('.title-container').append("<p>" + title + "</p>");
-                // $('.title-container').append("<img class='img' src=" + image + "></img>");
+
             }
         })
+    console.log(filmTitleArray)
+    console.log(filmImgArray)
 
+    // setTimeout( initialDisplay, 1000)
+    
+    function initialDisplay() {
+            $('.title').append("<p>" + filmTitleArray[0] + "</p>");
+            $('.film-container').append("<img class='film-container' src=" + filmImgArray[0] + "></img>");
+         
+    }
+    
+    if (filmImgArray.length  == 0) {
+        console.log('Array is empty')
+    } else {
+        initialDisplay()
+    }
+}
+    
+
+
+
+
+
+
+var filmNum = 0
+function testButton() {
+    filmNum++
+    $('.title').replaceWith("<p>" + filmTitleArray[filmNum] + "</p>");
+    $('.film-container').replaceWith("<img class='film-container' src=" + filmImgArray[filmNum] + "></img>");
+    console.log(filmNum)
 }
 
-console.log(filmTitleArray)
-console.log(filmImgArray)
+
+
 
